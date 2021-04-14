3597c721745f05d39eb8c7ffcd6472115cf35315
Scott DePouw <scott.depouw@gmail.com>
2021-04-14 07:44:39 -0400

    It's working.

--- src/app/state/message.effects.ts
+++ src/app/state/message.effects.ts
@@ -10,9 +10,12 @@ export class MessageEffects {
         () =>
             this.actions$.pipe(
                 ofType(displayMessage),
-                debounceTime(3000),
+                debounceTime(3000), // TODO: Inject timeout via displayMessage action?
                 tap(_ => this.store.dispatch(clearMessage()))
-            )
+            ),
+        // dispatch: false the re-dispatch of displayMessage.
+        // Since we're not piping into another Action to dispatch, it re-dispatches.
+        { dispatch: false }
     );
 
     constructor(
