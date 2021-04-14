a7a510ceb04677c95702707d427f4677fb971339
Scott DePouw <scott.depouw@gmail.com>
2021-04-14 06:57:51 -0400

    Display a timed success message after a successful Dispatch.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Actions, ofType } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
+import { debounceTime, tap } from 'rxjs/operators';
 import { selectCurrentAPICount } from './state/api-call-count.selectors';
-import { TodoItem } from './state/todo-model';
 import { clearTodos, getTodos, getTodosSuccess } from './state/todos.actions';
 import { selectAllTodoItems } from './state/todos.selectors';
 
@@ -14,8 +14,7 @@ import { selectAllTodoItems } from './state/todos.selectors';
 export class AppComponent implements OnInit {
   todoItems$ = this.store.pipe(select(selectAllTodoItems));
   numberOfAPICalls$ = this.store.pipe(select(selectCurrentAPICount));
-
-  directItems: TodoItem[];
+  showingOff = false;
 
   constructor(
     private store: Store,
@@ -24,7 +23,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.actions$.pipe(ofType(getTodosSuccess))
-      .subscribe(() => { console.log('Success Message could be displayed with this!'); });
+      .pipe(
+        tap(() => { this.showingOff = true; }),
+        debounceTime(3000),
+        tap(() => { this.showingOff = false; })
+      ).subscribe();
   }
 
   getTheGoods() {
