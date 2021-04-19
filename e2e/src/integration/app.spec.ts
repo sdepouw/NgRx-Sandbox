7885842ca6c31f3898beb419e602bd5d68543154
Scott DePouw <scott.depouw@gmail.com>
2021-04-19 07:29:29 -0400

    Install testing-library and adjust E2E spec to use it.

--- e2e/src/integration/app.spec.ts
+++ e2e/src/integration/app.spec.ts
@@ -1,14 +1,20 @@
 describe('Todo App Home Page', () => {
-  const getHeader = () => cy.get('h1');
-  const getGoodsButton = () => cy.get('button').first();
-  const getTodoListItems = () => cy.get('li');
+  const getHeader = () => cy.findByRole('heading', { name: /todos/i });
+  const getGoodsButton = () => cy.findByRole('button', { name: /goods/i });
+  const getClearButton = () => cy.findByRole('button', { name: /clear/i });
+  const getTodoListItems = () => cy.findByRole('list');
+  const getMessage = () => cy.findByRole('alert');
 
   it('should work', () => {
     cy.visit('/');
     getHeader().should('contain', 'My Todos');
 
     getGoodsButton().click();
-
     getTodoListItems().should('be.visible');
+
+    getClearButton().click();
+    getTodoListItems().should('not.be.visible');
+
+    getMessage().should('be.visible').should('not.exist');
   });
 });
