b46472d73e7bad355cba5c966620ae8f95dba43d
Scott DePouw <scott.depouw@gmail.com>
2021-04-18 20:32:43 -0400

    Uninstall Protractor, install Cypress.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,24 +1,21 @@
-import { browser, logging } from 'protractor';
-import { AppPage } from './app.po';
+describe('Todo App Home Page', () => {
+  // const page = new AppPage();
 
-describe('workspace-project App', () => {
-  const page = new AppPage();
+  // it('should display everything properly', async () => {
+  //   page.navigateTo();
+  //   expect(page.getTitleText()).toEqual('My Todos');
 
-  it('should display everything properly', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('My Todos');
+  //   page.getGoodsButton().click();
 
-    page.getGoodsButton().click();
+  //   // TODO: For some reason, this promise does not resolve until the displayMessage() dipsatch finishes.
+  //   expect(page.getTodoListItems().isPresent()).toBeTruthy();
+  // });
 
-    // TODO: For some reason, this promise does not resolve until the displayMessage() dipsatch finishes.
-    expect(page.getTodoListItems().isPresent()).toBeTruthy();
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  // afterEach(async () => {
+  //   // Assert that there are no errors emitted from the browser
+  //   const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+  //   expect(logs).not.toContain(jasmine.objectContaining({
+  //     level: logging.Level.SEVERE,
+  //   } as logging.Entry));
+  // });
 });
--- e2e/src/app.po.ts
+++
@@ -1,8 +0,0 @@
-import { browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
-
-export class AppPage {
-  navigateTo = (): Promise<unknown> => browser.get(browser.baseUrl) as Promise<unknown>;
-  getTitleText = (): Promise<string> => element(by.css('h1')).getText() as Promise<string>;
-  getGoodsButton = (): ElementFinder => element.all(by.css('button')).first();
-  getTodoListItems = (): ElementArrayFinder => element.all(by.css('li'));
-}
