bb84c593a83a84b54437036e9bbce2fa9ac17af0
Scott DePouw <scott.depouw@gmail.com>
2021-04-18 21:16:20 -0400

    Got Cypress test working.

--- e2e/src/integration/app.spec.ts
+++ e2e/src/integration/app.spec.ts
@@ -1,27 +1,14 @@
 describe('Todo App Home Page', () => {
+  const getHeader = () => cy.get('h1');
+  const getGoodsButton = () => cy.get('button').first();
+  const getTodoListItems = () => cy.get('li');
+
   it('should work', () => {
     cy.visit('/');
-    const header = cy.get('h1');
+    getHeader().should('contain', 'My Todos');
 
-    header.contains('todo');
+    getGoodsButton().click();
+
+    getTodoListItems().should('be.visible');
   });
-  // const page = new AppPage();
-
-  // it('should display everything properly', async () => {
-  //   page.navigateTo();
-  //   expect(page.getTitleText()).toEqual('My Todos');
-
-  //   page.getGoodsButton().click();
-
-  //   // TODO: For some reason, this promise does not resolve until the displayMessage() dipsatch finishes.
-  //   expect(page.getTodoListItems().isPresent()).toBeTruthy();
-  // });
-
-  // afterEach(async () => {
-  //   // Assert that there are no errors emitted from the browser
-  //   const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-  //   expect(logs).not.toContain(jasmine.objectContaining({
-  //     level: logging.Level.SEVERE,
-  //   } as logging.Entry));
-  // });
 });
