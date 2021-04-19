aa591b3f83ca5f8821ba88d149968cf976b60617
Scott DePouw <scott.depouw@gmail.com>
2021-04-19 19:24:24 -0400

    Need this because we're using router-outlet.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [],
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
