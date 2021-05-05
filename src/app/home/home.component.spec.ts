3dc9d1e3aa7581af2f7b8514dd6aab090b9c6667
Scott DePouw <scott.depouw@gmail.com>
2021-05-05 07:41:44 -0400

    Wrote failing Action subscription test.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,12 +1,28 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Action, StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { getTodosSuccess } from '@state-todos/todos.actions';
+import { displayMessage } from '@state/message.actions';
+import * as testHelpers from '@test-helpers';
+import { TodoListComponent } from '@todos/todo-list/todo-list.component';
+import { hot } from 'jasmine-marbles';
+import { MockComponent } from 'ng-mocks';
+import { Observable } from 'rxjs';
 import { HomeComponent } from './home.component';
+import { selectTodoTitle } from '@state-todos/todos.selectors';
+import { selectCurrentAPICount } from '@state/api-call-count.selectors';
+import { selectMessage } from '@state/message.selectors';
+import { isThePizzaReady } from '@state/pizza.selectors';
 
 describe('HomeComponent', () => {
   const initialState = {};
+  let mockStore: MockStore;
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let actions$: Observable<Action>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,17 +32,35 @@ describe('HomeComponent', () => {
         EffectsModule.forRoot(),
       ],
       declarations: [
-        HomeComponent
+        HomeComponent,
+        MockComponent(TodoListComponent)
       ],
       providers: [
-        provideMockStore({ initialState })
+        provideMockStore({ initialState }),
+        provideMockActions(() => actions$)
       ]
     }).compileComponents();
+
+    mockStore = testHelpers.createMockStoreWithDispatchSpy();
+    mockStore.overrideSelector(selectTodoTitle, 'Foo');
+    mockStore.overrideSelector(selectCurrentAPICount, 5);
+    mockStore.overrideSelector(selectMessage, 'Bar');
+    mockStore.overrideSelector(isThePizzaReady, true);
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(HomeComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  // TODO: This test is currently failing, because the Action subscription is never getting called.
+  xit('should display message when TODOs are gotten', () => {
+    actions$ = hot('----a--', { a: getTodosSuccess({ todoItems: [] }) });
+
+    fixture.detectChanges();
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith(displayMessage({ message: 'Successfully Got!' }));
   });
 });
